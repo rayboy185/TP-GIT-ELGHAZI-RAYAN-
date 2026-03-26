7951be4a6efcab21591a1060799e677e687c5747
elr8393@gmail.com <elr8393@gmail.com>
2026-03-26 10:20:14 +0100

    second git commit

---
+++ git2.java
@@ -0,0 +1,11 @@
+public class git2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        int age = 25;
+        String name = "Alice";
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
